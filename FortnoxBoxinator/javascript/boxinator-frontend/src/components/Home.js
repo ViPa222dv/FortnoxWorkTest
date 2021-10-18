d219e15108b4aa17ef2f3aeb4c20ecba913dbd87
Vilhelm <47038708+ViPa222dv@users.noreply.github.com>
2021-10-18 15:20:14 +0200

    Delete FortnoxBoxinator directory

--- FortnoxBoxinator/javascript/boxinator-frontend/src/components/Home.js
+++
@@ -1,5 +0,0 @@
-class Home extends Component {
-  render() {
-    return <H1>Home</H1>;
-  }
-}
--- FortnoxBoxinator/javascript/boxinator-frontend/src/reducers/countryReducer.js
+++
@@ -1,6 +0,0 @@
-//TODO Fix a reducer that creates the prize for the box based on country box weight * country multiplier
-/* Multiplier Sweden (1.3)  
-China (4)  
-Brazil (8.6) 
-Australia (7.2)
-*/
